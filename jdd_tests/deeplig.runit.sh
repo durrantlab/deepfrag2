27ede3861c9e6e357f4c7e5ad98e64a4d5f0c63b
jdurrant <durrantj@pitt.edu>
2021-12-02 23:19:31 -0500

    Update

--- jdd_tests/deeplig.runit.sh
+++ jdd_tests/deeplig.runit.sh
@@ -2,5 +2,7 @@ export NCCL_ASYNC_ERROR_HANDLING=1
 #wandb login
 echo START: $(date)
 # 
-NCCL_ASYNC_ERROR_HANDLING=1 python deeplig.py --csv ../data/moad/every.csv --data ../data/moad/BindingMoad2019/ --cache ./cache.json --gpus 1 --num_sanity_val_steps 0
+CUDA_LAUNCH_BLOCKING=1 NCCL_ASYNC_ERROR_HANDLING=1 python deeplig.py --csv ../data/moad/every.csv --data ../data/moad/BindingMoad2019/ --cache ./cache.json --gpus 1 --num_sanity_val_steps 0 --num_workers 0
+#16
+
 # --wandb_project 3aee432b3e7c672a3b2d2accf15b6b56a2770584 --num_workers 1
