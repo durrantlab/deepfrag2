ec23a53e3e4a06575f43a2ee44ec3eb34ec097ff
crjacas <cesarrjacas1985@gmail.com>
2022-10-31 01:34:16 -0400

    Minor changes. Closing multiprocessing flow

--- MainDF2.submit.sh
+++ MainDF2.submit.sh
@@ -9,15 +9,12 @@
 #SBATCH --partition=a100
 #SBATCH --gres=gpu:1
 
-. /ihome/jdurrant/durrantj/.bashrc
-
 module purge
 module load cuda/11.1.1
+module load pytorch_gpu/1.11.0
 
-export PATH=/ihome/jdurrant/durrantj/miniconda3/envs/deepfrag2/bin:/ihome/jdurrant/durrantj/miniconda3/condabin:${PATH}
-conda activate deepfrag2
-export LD_LIBRARY_PATH=/ihome/jdurrant/durrantj/miniconda3/envs/deepfrag2/lib:$LD_LIBRARY_PATH
+source activate deepfrag2cesar
 
-python MainDF2.py
+python MainDF2.py --csv /ihome/jdurrant/crg93/moad/every.csv --data /ihome/jdurrant/crg93/moad/BindingMOAD_2020 --save_splits /ihome/jdurrant/crg93/output/splits.json --default_root_dir /ihome/jdurrant/crg93/output --aggregation_3x3_patches mean --aggregation_loss_vector mean --max_epochs 30
 
 
