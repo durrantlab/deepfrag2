9c76876c6eab0c057a012503cc819110f4f82ce7
crjacas <cesarrjacas1985@gmail.com>
2022-12-22 01:39:52 -0500

    Stable version. Adding script for inference.

--- MainDF2.submit_inference.sh
+++ MainDF2.submit_inference.sh
@@ -19,7 +19,7 @@ source activate ${CONDA_ENV_NAME}
 cd ${DEEPFRAG_ROOT}
 
 #This command is recommended to perform a faster inference process
-python MainDF2.py --csv ${EVERY_CSV} --data ${BINDINGMOAD_DIR} --external_data ${EXTERNAL_DATA} --default_root_dir ${OUTPUT_TRAIN_DIR} --mode inference  --inference_rotations 8 --aggregation_rotations mean --load_newest_checkpoint True --inference_label_sets test
+python MainDF2.py --csv ${EVERY_CSV} --data ${BINDINGMOAD_DIR} --external_data ${EXTERNAL_DATA} --default_root_dir ${OUTPUT_FT_DIR} --mode inference  --inference_rotations 8 --aggregation_rotations mean --load_newest_checkpoint True --inference_label_sets test
 
 #This command will perform a slower inference process because it requires to compute fingerprints for fragments into MOAD database
-#python MainDF2.py --csv ${EVERY_CSV} --data ${BINDINGMOAD_DIR} --external_data ${EXTERNAL_DATA} --default_root_dir ${OUTPUT_TRAIN_DIR} --mode inference  --inference_rotations 8 --aggregation_rotations mean --load_newest_checkpoint True --inference_label_sets all
+#python MainDF2.py --csv ${EVERY_CSV} --data ${BINDINGMOAD_DIR} --external_data ${EXTERNAL_DATA} --default_root_dir ${OUTPUT_FT_DIR} --mode inference  --inference_rotations 8 --aggregation_rotations mean --load_newest_checkpoint True --inference_label_sets all
