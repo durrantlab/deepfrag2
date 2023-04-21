110a0415e866782d994299eb63c6e87f9e77528a
jdurrant <jacobdurrant@gmail.com>
2023-04-21 14:49:11 -0400

    Code for categorizing charged/uncharged works (tested).

--- collagen/external/moad/frag_substruct_detect.py
+++ collagen/external/moad/frag_substruct_detect.py
@@ -46,65 +46,119 @@ charged_substructs = [
         # thioic acid
         "[C,c,N,n](=[O,S])-[SX2,OX2]-[H]",
         "[c,n]-[SX2]-[H]",
+
+        # New ones adding for DeepFrag 
+        # 
+        # Aromatic nitrogens can often be positively charged near neutral pH.
+        # Count aromatic nitrogens that are bound to two atoms, neither of which
+        # is a hydrogen, as potentially positive.
+        "[n;H0;D2]",
+
+        # Above doesn't catch some sulfur-containing groups.
+        "S(=O)[O;H1]",
+
+        # To get, for exmaple, *OP(=S)(O[H])O[H]
+        "P(=S)[O;H1]"
+        "P(=O)[O;H1]"
+        "P(=O)[S;H1]"
+        "P(=S)[S;H1]"
     ]
 ]
 
 charged_substructs_to_ignore = [
-    Chem.MolFromSmarts(smi)
+    (Chem.MolFromSmarts(smi[0]), Chem.MolFromSmarts(smi[1]))
     for smi in [
         # azides
-        "N=[N+]=[N;X1]",
-        "N=[N+]=[N;H1;X2]",
+        ("N=[N+]=[N;X1]", "C"),
+        ("N=[N+]=[N;H1;X2]", "C"),
         # nitro
         # Actually, I think I'll count nitro groups as charged.
+        # Sulfonamide-like
+        ("S(=O)N", "S(=O)C"),
+        ("P(=O)N", "P(=O)C")
     ]
 ]
 
 
 def is_aromatic(mol: Chem.Mol) -> bool:
-    if mol.HasSubstructMatch(Chem.MolFromSmarts("a")):
-        # This probably isn't necessary given the next check, but leaving it
-        # here just in case.
-        return True
-    return any(atom.GetIsAromatic() for atom in mol.GetAtoms())
+    # Below is overkill. Could probably just keep the first one.
+    return (
+        len(mol.GetAromaticAtoms()) > 0
+        or mol.HasSubstructMatch(Chem.MolFromSmarts("a"))
+        or any(atom.GetIsAromatic() for atom in mol.GetAtoms())
+    )
 
 
 def is_charged(mol: Chem.Mol) -> bool:
-    # This code adapted from Dimorphite-DL
-    if any(
-        mol.HasSubstructMatch(charged_substruct)
-        for charged_substruct in charged_substructs
-    ):
-        return True
+    # If formal charge on any atom, then the molecule is charged. Easy solution.
+    for atom in mol.GetAtoms():
+        if atom.GetFormalCharge() != 0:
+            # print("1", "formal charge found", Chem.MolToSmiles(mol))
+            return True
 
-    # If no formal charge, then the molecule is not charged.
-    if all(atom.GetFormalCharge() == 0 for atom in mol.GetAtoms()):
-        return False
+    # if all(atom.GetFormalCharge() != 0 for atom in mol.GetAtoms()):
+    #     return True
 
-    # If here, then there is a charged atom. If there are no charged nitrogen
-    # atoms, count this molecule as charged.
-    if not any(
-        mol.HasSubstructMatch(charged_nitrogen_substruct)
-        for charged_nitrogen_substruct in charged_nitrogen_substructs
-    ):
-        return True
+    # If you get here, you need to consider the possibility that it could be
+    # ionizable, even though neutral in SMILES.
 
-    # The molecule has a charged nitrogen atom. Does it one of the prohibited
-    # substructures? If not, count it as charged.
-    if not any(
-        mol.HasSubstructMatch(charged_substruct_to_ignore)
-        for charged_substruct_to_ignore in charged_substructs_to_ignore
-    ):
-        return True
+    # Make copy of mol, so substitution doesn't change original
+    mol = Chem.Mol(mol)
 
-    # If here, there is a prohibited substructure. You must replace it with a
-    # carbon and reassess the charge.
-    for charged_substruct_to_ignore in charged_substructs_to_ignore:
+    # Go through and mask out certain nitrogens. There could be a prohibited
+    # substructure that appears to be a charged nitrogen (e.g., azide). You must
+    # replace it with a carbon.
+    for charged_substruct_to_ignore, replacement in charged_substructs_to_ignore:
         if mol.HasSubstructMatch(charged_substruct_to_ignore):
             mol = Chem.ReplaceSubstructs(
-                mol, charged_substruct_to_ignore, Chem.MolFromSmiles("C")
+                mol, charged_substruct_to_ignore, replacement
             )[0]
-            if is_charged(mol):
-                return True
 
+            # fix
+            mol.UpdatePropertyCache(strict=False)
+
+    for charged_substruct in charged_substructs:
+        if mol.HasSubstructMatch(charged_substruct):
+            # print("2", "charged substruct found", Chem.MolToSmiles(mol), Chem.MolToSmarts(charged_substruct))
+            return True
+
+    # print("3", "no charged substruct found", Chem.MolToSmiles(mol))
     return False
+
+    # if any(
+    #     mol.HasSubstructMatch(charged_substruct)
+    #     for charged_substruct in charged_substructs
+    # ):
+    #     return True
+
+
+    # If here, then there is a charged atom. If there are no charged nitrogen
+    # atoms, count this molecule as charged. Charged nitrogen atoms are handled
+    # separately below.
+    # if not any(
+    #     mol.HasSubstructMatch(charged_nitrogen_substruct)
+    #     for charged_nitrogen_substruct in charged_nitrogen_substructs
+    # ):
+    #     return True
+
+    # The molecule has a charged nitrogen atom. Does it one of the prohibited
+    # substructures (e.g., azide)? If not, count it as charged.
+    # if not any(
+    #     mol.HasSubstructMatch(charged_substruct_to_ignore)
+    #     for charged_substruct_to_ignore in charged_substructs_to_ignore
+    # ):
+    #     return True
+
+    # If here, there is a prohibited substructure that appears to be a charged
+    # nitrogen (e.g., azide). You must replace it with a carbon and reassess the
+    # charge.
+    # for charged_substruct_to_ignore in charged_substructs_to_ignore:
+    #     if mol.HasSubstructMatch(charged_substruct_to_ignore):
+    #         mol = Chem.ReplaceSubstructs(
+    #             mol, charged_substruct_to_ignore, Chem.MolFromSmiles("C")
+    #         )[0]
+    #         if is_charged(mol):
+    #             return True
+
+    # # If you get here, it's not charged.
+    # return False
