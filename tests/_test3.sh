2be573cffc19ec0d6a2cf9072b8ff5f13c989b3c
Durrant <durraj@g022.cm.cluster>
2023-04-27 04:02:25 +0000

    Updates to tests.

--- tests/_test3.sh
+++ tests/_test3.sh
@@ -9,7 +9,7 @@ $PYTHON_EXEC -u $MAIN_DF2_PY \
     --max_epochs 5 \
     --save_params ./3.finetune_moad.output/params.saved.json \
     --save_splits ./3.finetune_moad.output/splits.saved.json \
-    --model_for_warm_starting ./1.train_on_moad.output/model_mean_mean_train.pt \
+    --model_for_warm_starting ./1.train_on_moad.output/model_train.pt \
     --data_dir ./data_to_finetune/  `# Protein/ligands named like 1XDN_prot_123.pdb, 1XDN_lig_123.sdf` \
     --default_root_dir $(pwd)/3.finetune_moad.output/  `# The output directory` \
     --json_params common_params.json.inp \
