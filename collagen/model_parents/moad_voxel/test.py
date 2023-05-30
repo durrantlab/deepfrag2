f673848b326d16980bff455db448e5454b165441
jdurrant <jacobdurrant@gmail.com>
2023-05-30 15:52:36 -0400

    Minor update

--- collagen/model_parents/moad_voxel/test.py
+++ collagen/model_parents/moad_voxel/test.py
@@ -652,7 +652,7 @@ class MoadVoxelModelTest(object):
             max_pdbs_val=args.max_pdbs_val,
             max_pdbs_test=args.max_pdbs_test,
             split_method=args.split_method,
-            butina_cluster_cutoff=0.0, # TODO: Why hardcoded?
+            butina_cluster_cutoff=0.0, # Hardcoded because no need to split test set.
         )
 
         # You'll always need the test data. Note that ligands are not fragmented
