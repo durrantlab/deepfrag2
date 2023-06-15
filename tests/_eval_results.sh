b28343af55afbb4f8df8ea2fc0a7ad0a09b546c5
crjacas <cesarrjacas1985@gmail.com>
2023-06-15 18:00:12 -0400

    Updating to the last version on GitLab

---
+++ tests/_eval_results.sh
@@ -0,0 +1,20 @@
+# Create a function that detects if a file exists. If it does, it passes. Otherwise, it fails.
+function test_file_exists {
+    if [ -f $1 ]; then
+        echo "PASS: $1 exists"
+    else
+        echo "FAIL: $1 does not exist"
+    fi
+}
+
+# Now report results of tests
+echo
+echo "Results of tests"
+echo "================"
+
+test_file_exists ./1.train_on_moad.output/last.ckpt
+test_file_exists ./2.test_moad_trained.output/predictions_MOAD/mean/test_results-1.json
+test_file_exists ./3.finetune_moad.output/last.ckpt
+test_file_exists ./4.finetune_test.output/predictions_nonMOAD/mean/test_results-1.json
+test_file_exists ./5.inference.output/inference_out.smi
+test_file_exists ./6.inference_custom_set.output/predictions_CustomDataset/mean/test_results-1.json
