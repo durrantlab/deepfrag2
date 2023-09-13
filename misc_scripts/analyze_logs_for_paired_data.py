a15c5d59072852e0c64334b9acb5ddd05f4c2499
crjacas <cesarrjacas1985@gmail.com>
2023-09-12 20:27:56 -0400

    analyze logs of paired data

--- misc_scripts/analyze_logs_for_paired_data.py
+++ misc_scripts/analyze_logs_for_paired_data.py
@@ -4,24 +4,49 @@ import csv
 path = "pth/to/log/files"
 
 logs = {
-    "0_matched_assay_pdb-lig.log": {
-        "pos_info_receptor": 6,
-        "pos_info_ligand": 10,
-        "results": {}
-    },
-    "1_unmatched_assay_pdb-lig.log": {
+    "1_fail_match_FirstSmiles_PDBLigand.log": {
         "pos_info_receptor": 6,
         "pos_info_ligand": 11,
         "results": {}
     },
-    "2_unmatched_pdb-lig_fragment.log":  {
+    "2_fail_match_SecondSmiles_PDBLigand.log": {
+        "pos_info_receptor": 6,
+        "pos_info_ligand": 11,
+        "results": {}
+    },
+    "3_ligand_not_contain_parent.log":  {
         "pos_info_receptor": 5,
         "pos_info_ligand": 10,
         "results": {}
     },
-    "3_error_getting_3d_coordinates.log":  {
+    "4_ligand_not_contain_first-frag.log":  {
+        "pos_info_receptor": 5,
+        "pos_info_ligand": 10,
+        "results": {}
+    },
+    "5_ligand_not_contain_second-frag.log":  {
+        "pos_info_receptor": 5,
+        "pos_info_ligand": 10,
+        "results": {}
+    },
+    "6_error_getting_3d_coordinates_for_parent.log":  {
         "pos_info_receptor": 16,
         "pos_info_ligand": 9,
+        "results": {}
+    },
+    "7_error_getting_3d_coordinates_for_first-frag.log":  {
+        "pos_info_receptor": 16,
+        "pos_info_ligand": 9,
+        "results": {}
+    },
+    "8_error_getting_3d_coordinates_for_second-frag.log":  {
+        "pos_info_receptor": 16,
+        "pos_info_ligand": 9,
+        "results": {}
+    },
+    "9_finally_used.log": {
+        "pos_info_receptor": 6,
+        "pos_info_ligand": 10,
         "results": {}
     },
 }
@@ -30,7 +55,7 @@ for log_file_name in logs:
     log_file = os.path.abspath(os.path.join(path, log_file_name))
     with open(log_file) as reader_log:
         for line in reader_log:
-            if line.startswith("INFO") and ("Second" not in line):
+            if line.startswith("INFO"):
                 split_line = line.split(" ")
                 info_receptor = split_line[logs[log_file_name]["pos_info_receptor"]].replace("\n", "").replace("\"", "")
                 info_ligand = split_line[logs[log_file_name]["pos_info_ligand"]].replace("\n", "").replace("\"", "")
