746f510dec0e5e448a2d451ac492ed7b7dfb8f47
crjacas <cesarrjacas1985@gmail.com>
2023-09-18 09:51:21 -0400

    Minor changes

--- collagen/core/molecules/smiles_utils.py
+++ collagen/core/molecules/smiles_utils.py
@@ -4,6 +4,7 @@ from rdkit import Chem
 from rdkit.Chem.MolStandardize import rdMolStandardize
 from rdkit.Chem import rdmolops
 
+
 # From https://www.rdkit.org/docs/Cookbook.html
 def neutralize_atoms(mol: Chem.Mol) -> Chem.Mol:
     """Neutralize the molecule by adding/removing hydrogens.
@@ -43,7 +44,7 @@ def standardize_smiles(smiles: str) -> str:
         rdmol = Chem.MolFromSmiles(smiles)
 
         # Neutralize the molecule (charges)
-        neutralize_atoms(rdmol)
+        # neutralize_atoms(rdmol)
 
         rdmolops.Cleanup(rdmol)
         rdmolops.RemoveStereochemistry(rdmol)
