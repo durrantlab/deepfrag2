fc6be29845151c3780dc04811cd2f4cf00d36d8d
crjacas <cesarrjacas1985@gmail.com>
2024-04-21 23:45:38 -0400

    Changes to use a network to predict densities

--- apps/deepfrag/model_density_predictions.py
+++ apps/deepfrag/model_density_predictions.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
 from apps.deepfrag.model import DeepFragModel
 from typing import List, Tuple
 from collagen.external.moad.types import Entry_info
@@ -13,7 +12,7 @@ class VoxelAutoencoder(DeepFragModel):
 
         self.use_density_net = kwargs["use_density_net"]
         if self.use_density_net:
-            self.encoder_decoder = nn.Sequential(
+            self.density_encoder = nn.Sequential(
                 # Encoder
                 nn.Conv1d(16, 32, kernel_size=3, padding=1),
                 nn.MaxPool1d(2),
@@ -26,11 +25,12 @@ class VoxelAutoencoder(DeepFragModel):
                 nn.ReLU(),
                 nn.Flatten(),
                 nn.Dropout(),
-                nn.Linear(8192, self.fp_size),  # Here latent space
+                nn.Linear(8192, 512),  # Here latent space
                 nn.ReLU(),
-
+            )
+            self.density_decoder = nn.Sequential(
                 # Decoder
-                nn.Linear(self.fp_size, 8192),
+                nn.Linear(512, 8192),
                 nn.ReLU(),
                 nn.Unflatten(1, (128, 64)),
                 nn.ConvTranspose1d(
@@ -47,26 +47,33 @@ class VoxelAutoencoder(DeepFragModel):
             )
 
     def forward(self, voxel: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the model.
 
+        Args:
+            voxel (torch.Tensor): The voxel grid.
+
+        Returns:
+            torch.Tensor: The predicted fragment fingerprint, the recovered DF latent space, the DF latent space, and the density latent spce
+        """
         if not self.use_density_net:
             return super().forward(voxel)
 
         df_latent_space = self.encoder(voxel)
-        predicted_fps = self.deepfrag_after_encoder(df_latent_space)
 
-        new_latent_space = df_latent_space
+        aux_df_latent_space = df_latent_space
         if df_latent_space.size(0) < 16:
-            new_latent_space = torch.zeros(16, df_latent_space.size(1))
-            new_latent_space[:df_latent_space.size(0), :df_latent_space.size(1)] = df_latent_space
+            aux_df_latent_space = torch.zeros(16, df_latent_space.size(1))
+            aux_df_latent_space[:df_latent_space.size(0), :df_latent_space.size(1)] = df_latent_space
 
-        x = self.encoder_decoder(new_latent_space.unsqueeze(0)).squeeze(0)
+        den_latent_space = self.density_encoder(aux_df_latent_space.unsqueeze(0))
+        pred_df_latent_space = self.density_decoder(den_latent_space).squeeze(0)
 
-        if x.size(0) > df_latent_space.size(0):
-            new_x = torch.zeros(df_latent_space.size(0), df_latent_space.size(1))
-            new_x[:df_latent_space.size(0), :df_latent_space.size(1)] = x[:df_latent_space.size(0), :df_latent_space.size(1)]
-            x = new_x
+        if pred_df_latent_space.size(0) > df_latent_space.size(0):
+            aux = torch.zeros(df_latent_space.size(0), df_latent_space.size(1))
+            aux[:df_latent_space.size(0), :df_latent_space.size(1)] = pred_df_latent_space[:df_latent_space.size(0), :df_latent_space.size(1)]
+            pred_df_latent_space = aux
 
-        return predicted_fps, x, df_latent_space
+        return self.deepfrag_after_encoder(df_latent_space), pred_df_latent_space, df_latent_space, den_latent_space
 
     def training_step(
             self, batch: Tuple[torch.Tensor, torch.Tensor, List[Entry_info]], batch_idx: int
@@ -86,12 +93,12 @@ class VoxelAutoencoder(DeepFragModel):
 
         voxels, fps, entry_infos = batch
 
-        predicted_fps, x, df_latent_space = self(voxels)
+        predicted_fps, pred_df_latent_space, df_latent_space, _ = self(voxels)
 
         batch_size = voxels.shape[0]
 
         pred_loss = self.loss(predicted_fps, fps, entry_infos, batch_size)
-        densities_loss = F.mse_loss(x, df_latent_space)
+        densities_loss = nn.MSELoss(reduction='sum')(pred_df_latent_space, df_latent_space)
         loss = pred_loss + densities_loss
 
         self._mark_example_used("train", entry_infos)
@@ -115,12 +122,12 @@ class VoxelAutoencoder(DeepFragModel):
         else:
             voxels, fps, entry_infos = batch
 
-            predicted_fps, x, df_latent_space = self(voxels)
+            predicted_fps, pred_df_latent_space, df_latent_space, _ = self(voxels)
 
             batch_size = voxels.shape[0]
 
             pred_loss = self.loss(predicted_fps, fps, entry_infos, batch_size)
-            densities_loss = F.mse_loss(x, df_latent_space)
+            densities_loss = nn.MSELoss(reduction='sum')(pred_df_latent_space, df_latent_space)
             loss = pred_loss + densities_loss
 
             self._mark_example_used("val", entry_infos)
@@ -146,12 +153,12 @@ class VoxelAutoencoder(DeepFragModel):
 
         voxels, fps, entry_infos = batch
 
-        predicted_fps, x, df_latent_space = self(voxels)
+        predicted_fps, pred_df_latent_space, df_latent_space, den_latent_space = self(voxels)
 
         batch_size = voxels.shape[0]
 
         pred_loss = self.loss(predicted_fps, fps, entry_infos, batch_size)
-        densities_loss = F.mse_loss(x, df_latent_space)
+        densities_loss = nn.MSELoss(reduction='sum')(pred_df_latent_space, df_latent_space)
         loss = pred_loss + densities_loss
 
         self._mark_example_used("test", entry_infos)
@@ -159,8 +166,7 @@ class VoxelAutoencoder(DeepFragModel):
         self.log("test_loss", loss, batch_size=batch_size)
 
         pred = torch.zeros(predicted_fps.size(0), predicted_fps.size(1))
-        x = self.deepfrag_after_encoder(x)
-        torch.add(input=predicted_fps, other=x, out=pred)
+        torch.add(input=predicted_fps, other=self.deepfrag_after_encoder(den_latent_space), out=pred)
         torch.div(input=pred, other=2, out=pred)
 
         # Drop (large) voxel input, return the predicted and target fingerprints.
