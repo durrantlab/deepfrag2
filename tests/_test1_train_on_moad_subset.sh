fc4a1be2b03e0dce021eae7e8a6898571fc77dde
jdurrant <jacobdurrant@gmail.com>
2025-09-02 14:56:46 -0400

    Adding tiny subset of moad for testing

--- tests/_test1_train_on_moad_subset.sh
+++ tests/_test1_train_on_moad_subset.sh
@@ -1,20 +1,18 @@
-. _init.sh
+echo "Train on a small subset of the Binding MOAD"
 
-echo "Train on a small subset of the Binding MOAD (for testing; --max_pdbs_train 100, --max_pdbs_val 100)"
-
+rm -rf 1.OUT-python_out.txt 1.train_on_moad.output
+rm -rf moad/BindingMOAD_2020_mini/*pkl
 mkdir -p 1.train_on_moad.output
 
-$PYTHON_EXEC -u $MAIN_DF2_PY \
+python -u ../MainDF2.py \
     --mode train_on_moad \
     --max_epochs 3 \
     --save_params ./1.train_on_moad.output/params.saved.json \
     --save_splits ./1.train_on_moad.output/splits.saved.json \
-    --csv $MOAD_DIR/${EVERY_CSV_BSNM} \
-    --data_dir $MOAD_DIR/ \
+    --csv moad/every.csv \
+    --data_dir moad/ \
     --cache ./1.train_on_moad.output/every_csv.cache.json \
     --default_root_dir $(pwd)/1.train_on_moad.output/  `# The output directory` \
-    --max_pdbs_train 100  `# Train on a small subset of the Binding MOAD` \
-    --max_pdbs_val 100  `# Validate on a small subset of the Binding MOAD` \
     --json_params common_params.json.inp \
     --cache_pdbs_to_disk \
     | tee 1.OUT-python_out.txt
--- tests/clean.sh
+++ tests/clean.sh
@@ -1,3 +1,4 @@
+rm -rf moad/BindingMOAD_2020_mini/*pkl
 rm -f ?.OUT-*
 rm -rf ?.*.output
 rm -f ./every_csv.cache.json
