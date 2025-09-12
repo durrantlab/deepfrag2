cf56d6dc9c755d8c2f2c92ed0872ce6878172e4c
jdurrant <jacobdurrant@gmail.com>
2025-09-12 05:31:22 -0400

    Improved test to download fragment library

--- tests/_test6_inference_on_custom_set.sh
+++ tests/_test6_inference_on_custom_set.sh
@@ -12,7 +12,7 @@ python -u ../MainDF2.py \
     --default_root_dir $(pwd)/6.inference_custom_set.output/ \
     --rotations 2 \
     --load_checkpoint gte_4_best \
-    --inference_label_sets ./data_for_inference/label_set.smi \
+    --inference_label_sets gte_4_all \
     --cache_pdbs_to_disk \
     --cache None \
     | tee 5.OUT-python_out.txt
